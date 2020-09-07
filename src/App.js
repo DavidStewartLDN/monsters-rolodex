a5f282752ce3b2001a6f9b44cbd68d20863756e4
DavidStewartLDN <davidstewart.work@gmail.com>
2020-09-07 16:06:48 +0100

    First commit - editing App.js page with onClick state chnge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      string: "I'm sorry Dave, I'm afraid I can't let you do that."
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {this.state.string}
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {/* Put in all the properties that you want to change within state inside the change of state. key-value pairs */}
+          <button onClick={() => this.setState({string: 'Hello Miguel'})}>Change text</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
