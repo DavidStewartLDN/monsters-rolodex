07f5e1de55ad298c55f5453edf7623b77148998f
DavidStewartLDN <davidstewart.work@gmail.com>
2020-09-07 20:14:42 +0100

    Created CardList component and listed names using children.props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component'
 import './App.css';
 
 // function App() {
@@ -42,12 +42,13 @@ componentDidMount() {
 
 
   render() {
-    console.log(this.state)
     return (
       <div className="App">
-      {
-        this.state.monsters.map(monster => <h1 key={monster.id}> {monster.name}</h1>)
-      }
+      <CardList name="Mike"> 
+        {
+          this.state.monsters.map(monster => <h1 key={monster.id}> {monster.name}</h1>)
+        }
+      </CardList>
       </div>
     );
   }
