3c39ecb4bd489fbdde1dc54831fda2ae16525b06
DavidStewartLDN <davidstewart.work@gmail.com>
2020-09-08 18:51:46 +0100

    Added arrow functions using lexical scoping for handleChange

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component'
 import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 class App extends Component {
   constructor() {
@@ -30,6 +10,7 @@ class App extends Component {
       monsters: [],
       searchField: ''
     }
+    
   }
   
 /*  When this component mounts, e.g. puts the componet on the page. It calls whatever is inside the block of code */
@@ -40,6 +21,10 @@ componentDidMount() {
   .then(users => this.setState({monsters: users}))
 }
 
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value})
+  }
+
 
 
   render() {
@@ -48,10 +33,9 @@ componentDidMount() {
       monster.name.toLowerCase().includes(searchField.toLowerCase()));
     return (
       <div className="App">
-      <input
-        type='search'
-        placeholder='Search Monsters'
-        onChange={e => this.setState({searchField: e.target.value})}
+      <SearchBox 
+        placeholder = 'Search Monsters'
+        handleChange={this.handleChange}
       />
       <CardList monsters={filteredMonsters}></CardList>
       </div>
