00d8e5f796f8b520686c7ecc5b3cbf2cde7401eb
DavidStewartLDN <davidstewart.work@gmail.com>
2020-09-09 16:48:14 +0100

    Updates

---
+++ precache-manifest.06e637d5a361c890476f5993be5ca572.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eab1d3e7beb3718821dc56f4305eb4c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "039e97b5998bc1850aeb",
+    "url": "/monsters-rolodex/static/css/main.2892774e.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "039e97b5998bc1850aeb",
+    "url": "/monsters-rolodex/static/js/main.4d602ff6.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
